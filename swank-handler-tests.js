a81837aabd6c39a8d83a23a79743c6492dc1db1f
Ivan Shvedunov <ivan4th@gmail.com>
2010-11-25 09:55:13 +0300

    Added support for 'remotes' in swank-handler.js.

--- swank-handler-tests.js
+++ swank-handler-tests.js
@@ -1,10 +1,12 @@
 // -*- mode: js2; moz-minor-mode: nil; jsc-minor-mode: t -*-
 var swh = require("./swank-handler");
 var readFromString = require("./lisp").readFromString;
+var util = require("util");
 var assert = require("assert");
 
 var expected = [];
-var handler = new swh.Handler(new swh.Executive({ pid: 4242 }));
+var executive = new swh.Executive({ pid: 4242 });
+var handler = new swh.Handler(executive);
 
 handler.on(
   "response", function (response) {
@@ -18,11 +20,20 @@ handler.on(
       assert.equal(expectedResponse, response);
   });
 
+function expect () {
+  for (var i = 0; i < arguments.length; ++i)
+    expected.push(arguments[i]);
+}
+
+function verifyExpectations () {
+  assert.equal(0, expected.length);
+}
+
 function request (str) {
   for (var i = 1; i < arguments.length; ++i)
     expected.push(arguments[i]);
   handler.receive(readFromString(str));
-  assert.equal(0, expected.length);
+  verifyExpectations();
 }
 
 request('(:emacs-rex (swank:connection-info) "COMMON-LISP-USER" t 1)',
@@ -61,3 +72,120 @@ request('(:emacs-rex (swank:listener-eval "zzz") "JS" :repl-thread 9)',
         '(:return (:ok nil) 9)');
 
 // TBD: debugger
+
+function FakeRemote (name) {
+  this.name = name;
+};
+
+util.inherits(FakeRemote, swh.Remote);
+
+FakeRemote.prototype.kind = function kind () {
+  return "test";
+};
+
+FakeRemote.prototype.id = function id () {
+  return this.name;
+};
+
+FakeRemote.prototype.evaluate = function evaluate (id, str) {
+  this.sendResult(id, [ "R:" + this.name + ":" + str.replace(/^\s*|\s*$/g, "") ]);
+};
+
+request('(:emacs-rex (js:list-remotes) "JS" :repl-thread 10)',
+        '(:return (:ok ((1 :direct "node.js" t))) 10)');
+
+expect('(:write-string "Remote attached: (test) test/localhost:8080")');
+var r1 = new FakeRemote("test/localhost:8080");
+executive.attachRemote(r1);
+verifyExpectations();
+
+request('(:emacs-rex (js:list-remotes) "JS" :repl-thread 11)',
+        '(:return (:ok ((1 :direct "node.js" t) (2 :test "test/localhost:8080" nil))) 11)');
+
+expect('(:write-string "Remote attached: (test) test/localhost:9999")');
+var r2 = new FakeRemote("test/localhost:9999");
+executive.attachRemote(r2);
+verifyExpectations();
+
+request('(:emacs-rex (js:list-remotes) "JS" :repl-thread 12)',
+        '(:return (:ok ((1 :direct "node.js" t) ' +
+        '(2 :test "test/localhost:8080" nil) ' +
+        '(3 :test "test/localhost:9999" nil))) 12)');
+
+request('(:emacs-rex (swank:listener-eval "3 * 10\n") "JS" :repl-thread 13)',
+        '(:return (:ok (:values "30")) 13)');
+
+request('(:emacs-rex (js:select-remote 2 nil) "JS" :repl-thread 14)',
+        '(:write-string "Remote selected: (test) test/localhost:8080")',
+        '(:return (:ok nil) 14)');
+
+request('(:emacs-rex (js:list-remotes) "JS" :repl-thread 15)',
+        '(:return (:ok ((1 :direct "node.js" nil) ' +
+        '(2 :test "test/localhost:8080" t) ' +
+        '(3 :test "test/localhost:9999" nil))) 15)');
+
+request('(:emacs-rex (swank:listener-eval "3 * 10\n") "JS" :repl-thread 16)',
+        '(:return (:ok (:values "R:test/localhost:8080:3 * 10")) 16)');
+
+expect('(:write-string "Remote detached: (test) test/localhost:8080")',
+       '(:write-string "Remote selected: (direct) node.js")');
+r1.disconnect();
+verifyExpectations();
+
+request('(:emacs-rex (swank:listener-eval "3 * 10\n") "JS" :repl-thread 17)',
+        '(:return (:ok (:values "30")) 17)');
+
+request('(:emacs-rex (js:list-remotes) "JS" :repl-thread 18)',
+        '(:return (:ok ((1 :direct "node.js" t) ' +
+        '(3 :test "test/localhost:9999" nil))) 18)');
+
+expect('(:write-string "Remote detached: (test) test/localhost:9999")');
+r2.disconnect();
+verifyExpectations();
+
+request('(:emacs-rex (swank:listener-eval "3 * 10\n") "JS" :repl-thread 19)',
+        '(:return (:ok (:values "30")) 19)');
+
+request('(:emacs-rex (js:list-remotes) "JS" :repl-thread 20)',
+        '(:return (:ok ((1 :direct "node.js" t))) 20)');
+
+request('(:emacs-rex (js:select-remote 2 nil) "JS" :repl-thread 21)',
+        '(:write-string "WARNING: bad remote index")',
+        '(:return (:ok nil) 21)');
+
+request('(:emacs-rex (swank:listener-eval "3 * 10\n") "JS" :repl-thread 22)',
+        '(:return (:ok (:values "30")) 22)');
+
+request('(:emacs-rex (js:select-remote 1 nil) "JS" :repl-thread 23)',
+        '(:write-string "WARNING: remote already selected: (direct) node.js")',
+        '(:return (:ok nil) 23)');
+
+// TBD: use ## instead of numbers in the tests above (request() should take care of it)
+// TBD: test output from an inactive remote
+// TBD: test remote autoselection (pass t as second argument of js:select-remote)
+// TBD: test remote reattachment (the original one should be disconnected & notified about that)
+// TBD: change prompt upon remote selection
+// TBD: test remote disconnection (disconnectRemote) - both for active and inactive remotes
+// TBD: remote-based prompt (add package() to remote or something like that)
+// TBD: restrict remote names received from the browser (length, chars)
+// TBD: remote name should be formed on the server side using small client side id
+// TBD: are out-of-order results for :emacs-rex ok? look at slime sources
+
+/*
+
+list/select remotes along the lines of
+
+(defun slime-delete-system-fasls (name)
+  "Delete FASLs produced by compiling a system."
+  (interactive (list (slime-read-system-name)))
+  (slime-repl-shortcut-eval-async
+   `(swank:delete-system-fasls ,name)
+   'message))
+
+(defslime-repl-shortcut slime-repl-delete-system-fasls ("delete-system-fasls")
+  (:handler 'slime-delete-system-fasls)
+  (:one-liner "Delete FASLs of an ASDF system."))
+
+catching errors on the client: window.onerror
+http://stackoverflow.com/questions/951791/javascript-global-error-handling
+*/
--- swank-handler.js
+++ swank-handler.js
@@ -3,6 +3,7 @@ var EventEmitter = require("events").EventEmitter;
 var Script = process.binding('evals').Script;
 var evalcx = Script.runInContext;
 var util = require("util");
+var assert = process.assert;
 var lisp = require("./lisp");
 var S = lisp.S, consp = lisp.consp, car = lisp.car, cdr = lisp.cdr,
     repr = lisp.repr, fromLisp = lisp.fromLisp, toLisp = lisp.toLisp;
@@ -34,7 +35,14 @@ Handler.prototype.receive = function receive (message) {
     }
     throw e;
   }
+
   var r = { status: ":ok", result: null };
+  var self = this;
+  var cont = function cont () {
+    self.sendResponse({ r: r, id: d.id },
+                      [S(":return"), ">:r", ["S:status", "_:result"], "N:id"]);
+  };
+
   switch (d.form.name) {
   case "swank:connection-info":
     r.result = toLisp(this.executive.connectionInfo(),
@@ -50,6 +58,33 @@ Handler.prototype.receive = function receive (message) {
   case "swank:autodoc":
     r.result = S(":not-available");
     break;
+  case "js:list-remotes":
+    // FIXME: support 'list of similar elements' type spec
+    r.result = toLisp(
+      this.executive.listRemotes().map(
+        function (item) {
+          return toLisp(item, ["N:index", "K:kind", "s:id", "B:isActive"]);
+        }, this));
+    break;
+  case "js:select-remote":
+    if (d.form.args.length != 2) {
+      console.log("bad args len for SWANK:SELECT-REMOTE -- %s", d.form.args.length);
+      return; // FIXME
+    }
+    var remoteIndex, sticky;
+    try {
+      // FIXME: args should be a cons / NIL
+      remoteIndex = fromLisp(d.form.args[0], "N");
+      sticky = fromLisp(d.form.args[1]);
+    } catch (e) {
+      if (e instanceof TypeError) {
+        console.log("can't parse arg -- %s", d.form.args[0]);
+        return; // FIXME
+      }
+      throw e;
+    }
+    this.executive.selectRemote(remoteIndex, sticky);
+    break;
   case "swank:listener-eval":
     if (d.form.args.length != 1) {
       console.log("bad args len for SWANK:LISTENER-EVAL -- %s", d.form.args.length);
@@ -64,15 +99,17 @@ Handler.prototype.receive = function receive (message) {
       }
       throw e;
     }
-    var values = this.executive.listenerEval(expr);
-    if (values.length)
-      r.result = toLisp({ values: values }, [S(":values"), "R:values"]);
-    break;
+    this.executive.listenerEval(
+      expr, function (values) {
+        if (values.length)
+          r.result = toLisp({ values: values }, [S(":values"), "R:values"]);
+        cont();
+      });
+    return;
   default:
     // FIXME: handle unknown commands
   }
-  this.sendResponse({ r: r, id: d.id },
-                    [S(":return"), ">:r", ["S:status", "_:result"], "N:id"]);
+  cont();
 };
 
 Handler.prototype.output = function output (str) {
@@ -84,15 +121,53 @@ Handler.prototype.sendResponse = function sendResponse(response, spec)
   this.emit("response", repr(toLisp(response, spec || "@")));
 };
 
-function Executive (options) {
-  options = options || {};
-  this.pid = options.hasOwnProperty("pid") ? options.pid : null;
-  this.setupContext();
+function Remote () {};
+
+util.inherits(Remote, EventEmitter);
+
+Remote.prototype.kind = function kind () {
+  throw new Error("must override Remote.prototype.kind()");
 };
 
-util.inherits(Executive, EventEmitter);
+Remote.prototype.id = function id () {
+  throw new Error("must override Remote.prototype.id()");
+};
 
-Executive.prototype.setupContext = function setupContext () {
+Remote.prototype.evaluate = function evaluate (id, str) {
+  throw new Error("must override Remote.prototype.evaluate()");
+};
+
+Remote.prototype.fullName = function fullName () {
+  return "(" + this.kind() + ") " + this.id();
+};
+
+Remote.prototype.disconnect = function disconnect () {
+  this.emit("disconnect");
+};
+
+Remote.prototype.detachSelf = function detachSelf () {
+  this.removeAllListeners("output");
+  this.removeAllListeners("disconnect");
+  this.removeAllListeners("result");
+};
+
+Remote.prototype.output = function output (str) {
+  this.emit("output", String(str));
+};
+
+Remote.prototype.setIndex = function setIndex (n) {
+  this._index = n;
+};
+
+Remote.prototype.index = function index () {
+  return this._index;
+};
+
+Remote.prototype.sendResult = function sendResult (id, values) {
+  this.emit("result", id, values);
+};
+
+function DefaultRemote () {
   this.context = Script.createContext();
   for (var i in global) this.context[i] = global[i];
   this.context.module = module;
@@ -100,9 +175,89 @@ Executive.prototype.setupContext = function setupContext () {
   var self = this;
   this.context._swank = {
     output: function output (arg) {
-      self.emit("output", "" + arg);
+      self.output(arg);
     }
   };
+}
+
+util.inherits(DefaultRemote, Remote);
+
+DefaultRemote.prototype.kind = function kind () {
+  return "direct";
+};
+
+DefaultRemote.prototype.id = function id () {
+  return "node.js";
+};
+
+DefaultRemote.prototype.evaluate = function evaluate (id, str) {
+  var r;
+  try {
+    r = evalcx(str, this.context, "repl");
+  } catch (e) {
+    r = undefined;
+    this.output(e.stack);
+  }
+  this.sendResult(id, r === undefined ? [] : [util.inspect(r)]);
+};
+
+// TBD: rename Executive to Dispatcher
+function Executive (options) {
+  options = options || {};
+  this.pid = options.hasOwnProperty("pid") ? options.pid : null;
+  this.remotes = [];
+  this.attachRemote(new DefaultRemote());
+  this.activeRemote = this.remotes[0];
+  this.pendingRequests = {};
+};
+
+util.inherits(Executive, EventEmitter);
+
+Executive.nextId = 1; // request id counter is global in order to avoid inter-connection conflicts
+
+Executive.nextRemoteIndex = 1;
+
+Executive.prototype.attachRemote = function attachRemote (remote) {
+  assert(this.remotes.indexOf(remote) < 0);
+  remote.setIndex(Executive.nextRemoteIndex++);
+
+  var self = this;
+  remote.on(
+    "output", function (str) {
+      if (remote == self.activeRemote)
+        self.emit("output", str);
+    });
+  remote.on(
+    "disconnect", function (str) {
+      self.handleDisconnectRemote(remote);
+    });
+  remote.on(
+    "result", function (id, values) {
+      if (!self.pendingRequests[id]) {
+        emit("output", "WARNING: received late result from " + remote.fullName());
+        return;
+      }
+      try {
+        self.pendingRequests[id](values);
+      } finally {
+        delete self.pendingRequests[id];
+      }
+    });
+  this.remotes.push(remote);
+  this.emit("output", "Remote attached: " + remote.fullName());
+};
+
+Executive.prototype.handleDisconnectRemote = function handleDisconnectRemote (remote) {
+  remote.detachSelf();
+  var index = this.remotes.indexOf(remote);
+  if (index < 0) {
+    this.emit("output", "WARNING: disconnectRemote() called for an unknown remote: " + remote.fullName());
+    return;
+  }
+  this.remotes.splice(index, 1);
+  this.emit("output", "Remote detached: " + remote.fullName());
+  if (remote == this.activeRemote)
+    this.selectRemote(this.remotes[0].index());
 };
 
 Executive.prototype.connectionInfo = function connectionInfo () {
@@ -115,16 +270,37 @@ Executive.prototype.createRepl = function createRepl () {
   return { packageName: "JS", prompt: "JS" };
 };
 
-Executive.prototype.listenerEval = function listenerEval (str) {
-  var r;
-  try {
-    r = evalcx(str, this.context, "repl");
-  } catch (e) {
-    r = undefined;
-    this.emit("output", e.stack);
+Executive.prototype.listenerEval = function listenerEval (str, cont) {
+  var id = Executive.nextId++;
+  this.pendingRequests[id] = cont;
+  this.activeRemote.evaluate(id, str);
+};
+
+Executive.prototype.listRemotes = function listRemotes () {
+  return this.remotes.map(
+    function (remote) {
+      return { index: remote.index(), kind: remote.kind(), id: remote.id(),
+               isActive: remote === this.activeRemote };
+    }, this);
+};
+
+Executive.prototype.selectRemote = function selectRemote (index, sticky) {
+  // TBD: sticky support (should autoselect the remote with message upon attachment)
+  for (var i = 0; i < this.remotes.length; ++i) {
+    var remote = this.remotes[i];
+    if (remote.index() == index) {
+      if (remote == this.activeRemote) {
+        this.emit("output", "WARNING: remote already selected: " + remote.fullName());
+        return;
+      }
+      this.activeRemote = remote;
+      this.emit("output", "Remote selected: " + remote.fullName());
+      return;
+    }
   }
-  return r === undefined ? [] : [util.inspect(r)];
+  this.emit("output", "WARNING: bad remote index");
 };
 
 exports.Handler = Handler;
+exports.Remote = Remote;
 exports.Executive = Executive;
