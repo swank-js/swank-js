9b02c50ea6373810b3c70e439ff7bb82ccd78a2d
Robert Krahn <robert.krahn@gmail.com>
2012-01-16 08:54:31 +0100

    Loading swank-js scripts dynamically to make it easier to embed it in HTML pages.

---
+++ client/swank-js-inject.js
@@ -0,0 +1,32 @@
+
+/*
+ * Add this script to your html file to connect to swank-js
+ */
+
+
+// TODO don't hardcode swank server
+var swank_server = "http://localhost:8009/";
+
+function load(url, requirement) {
+  if (requirement) {
+    var fulfilled = requirement.call(window);
+    if (!fulfilled) {
+      window.setTimeout(function() { load(url, requirement); }, 100);
+      return;
+    }
+  }
+  var script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.setAttribute('src', swank_server + url);
+  document.head.appendChild(script);
+}
+
+function loadTests() {
+  var query = document.location.toString().split("?")[1];
+  return query && query.match(/swank-js-tests/);
+}
+
+load("swank-js/json2.js");
+load("socket.io/socket.io.js");
+load("swank-js/stacktrace.js");
+load("swank-js/swank-js.js", function() { return !!window.io; });
